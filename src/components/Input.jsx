19b32a246b208ecfa22b5ae35c2d387e4182afe9
Daniela Leiva <dgleivaencina@gmail.com>
2022-12-26 22:16:03 -0300

    problema con la validacion de password solucionado

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,30 +4,28 @@ import { useState } from "react";
 
 const Input = () => {
 const [nombre, setNombre] = useState('');
-const [pass, setPass] = useState('');
+const [pass, setPass] = useState();
 
+function validarPass(name, value){
+  console.log("validarPass esta funcionando", name, value);
+  if(value === '252525' && name ==='pass'){
+    setBoton(true);
+    return;
+  }else{setBoton(false);
+  }
+}
 //Estado boton
-const [boton, setBoton] = useState(false)
-const validarPass = (e) => {
-    
-    
-    //Validacion
-    if(pass === '252525'){
-        setBoton(true);
-        return;
-    }
-
-};
+const [boton, setBoton] = useState(false);
 
   return (
-    <form >
+    <form onChange={(e)=>validarPass(e.target.name, e.target.value)}>
       <div className="form-group">
         <label>Nombre</label>
         <input type="text" name="nombre" className="form-control" onChange={(e) => setNombre(e.target.value)} value={nombre} />
       </div>
       <div className="form-group">
         <label>Contraseña</label>
-        <input type="password" name="pass" className="form-control"  onChange={(e) => validarPass(setPass(e.target.value))} value={pass}/>
+        <input type="password" name="pass" className="form-control" onChange={(e) => setPass(e.target.value)}  value={pass}/>
       </div>
     {boton ? <Boton/> :null}
     </form>
